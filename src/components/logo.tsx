e3618bffb983b59f16f2d3d728dedf71c9e031ea
Albin Shaji <albinalby24@gmail.com>
2025-09-20 10:51:37 +0000

    change color of pearl 2025  in top bar

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -6,7 +6,7 @@ export default function Logo({ className }: { className?: string }) {
   return (
     <Link href="/" className={cn("flex items-center gap-2 text-xl font-bold font-headline tracking-tight", className)}>
       <Image src="https://upload.wikimedia.org/wikipedia/en/e/e3/Kcym_state_logo.png" alt="Pearl 2025 Logo" width={32} height={32} className="rounded-md" />
-      <span>Pearl 2025</span>
+      <span className="text-primary">Pearl 2025</span>
     </Link>
   );
 }
